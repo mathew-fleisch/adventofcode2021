43c0c5848ebdf66e52dd7f29414bb3de46c441ad
mathew-fleisch <mathew.fleisch@gmail.com>
2021-12-01 10:01:01 -0800

    Refactor due to Adrian's hatred towards me

--- 01/challenge1.sh
+++ 01/challenge1.sh
@@ -2,20 +2,26 @@
 #shellcheck disable=SC2086
 
 inputFile=${1:-input.txt}
-values=$(cat $inputFile)
-lastValue=9999999
+IFS=$'\n' read -d '' -r -a values < $inputFile;
+inputLength=${#values[@]}
+echo "Lines: $inputLength"
+lastValue=0
 increases=0
 track=0
-for val in $values; do
+while IFS= read -r val; do
   direction="-"
   track=$((track+1))
-  if [ $val -gt $lastValue ]; then
-    increases=$((increases+1))
-    direction="+"
+  if [ $track -ne 1 ]; then
+    if [ $val -gt $lastValue ]; then
+      increases=$((increases+1))
+      direction="+"
+    fi
+  else
+    direction="N/A"
   fi
 
   echo "$val $direction"
   lastValue=$val
-done
+done < $inputFile
 
 echo "Increases: $increases"
