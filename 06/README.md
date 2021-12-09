e8b63d1c7e3ccd4cb2c10949b40f1163dfc24f56
mathew-fleisch <mathew.fleisch@gmail.com>
2021-12-08 21:56:49 -0800

    Day 6: Lanternfish

--- 06/README.md
+++ 06/README.md
@@ -67,3 +67,9 @@ Find a way to simulate lanternfish. How many lanternfish would there be after 80
 Suppose the lanternfish live forever and have unlimited food and space. Would they take over the entire ocean?
 
 After 256 days in the example above, there would be a total of 26984457539 lanternfish!
+
+### Usage
+
+```bash
+./challenge1.sh ./input.txt
+```
--- 06/challenge1.sh
+++ 06/challenge1.sh
@@ -21,48 +21,72 @@ convertsecs() {
   printf "%02d:%02d:%02d\n" $h $m $s
 }
 
-lanternFish=()
+[ $DEBUG -eq 1 ] && echo "$values"
+
+fi=0
+fh=0
+fg=0
+ff=0
+fd=0
+fc=0
+fb=0
+fa=0
+ft=0
 for fish in ${values//,/ }; do
-  # echo "$fish"
-  lanternFish+=("$fish")
+  case $fish in
+      6)
+        fg=$((fg+1))
+        ;;
+      5)
+        ff=$((ff+1))
+        ;;
+      4)
+        fe=$((fe+1))
+        ;;
+      3)
+        fd=$((fd+1))
+        ;;
+      2)
+        fc=$((fc+1))
+        ;;
+      1)
+        fb=$((fb+1))
+        ;;
+      0)
+        fa=$((fa+1))
+        ;;
+  esac
 done
-unset fish
-echo "${lanternFish[*]}" | tr ' ' ','
 
 
 for ((day=1; day<=days; day++)); do
-  moreDifferentFish=()
-  newFish=0
-  numFish=${#lanternFish[*]}
+  numFish=$((fa + fb + fc + fd + fe + ff + fg + fh + fi))
   now=$(date +%s)
   diff=$((now-started))
   [ $DEBUG -eq 1 ] && echo "Day $day: $numFish $(convertsecs $diff)"
-  
-  for ((fish=0; fish<numFish; fish++)); do
-    disFish=${lanternFish[fish]}
-    datFish=$((disFish-1))
-    if [ $datFish -lt 0 ]; then
-      datFish=6
-      newFish=$((newFish+1))
-    fi
-    moreDifferentFish[$fish]=$datFish
-  done
-
-  for ((tng=0; tng<newFish; tng++)); do
-    moreDifferentFish+=(8)
-  done
-  unset lanternFish
-  lanternFish=("${moreDifferentFish[@]}")
-  unset moreDifferentFish
-  unset datFish
-  unset disFish
-  unset newFish
-
-  # [ $DEBUG -eq 1 ] && echo "${lanternFish[*]}" | tr ' ' ','
+  [ $fa -gt 0 ] && ft=$fa || ft=0
+  fa=$fb
+  fb=$fc
+  fc=$fd
+  fd=$fe
+  fe=$ff
+  ff=$fg
+  fg=$((ft+fh))
+  fh=$fi
+  fi=$ft
+  [ $DEBUG -eq 1 ] && echo "f[0]: $fa"
+  [ $DEBUG -eq 1 ] && echo "f[1]: $fb"
+  [ $DEBUG -eq 1 ] && echo "f[2]: $fc"
+  [ $DEBUG -eq 1 ] && echo "f[3]: $fd"
+  [ $DEBUG -eq 1 ] && echo "f[4]: $fe"
+  [ $DEBUG -eq 1 ] && echo "f[5]: $ff"
+  [ $DEBUG -eq 1 ] && echo "f[6]: $fg"
+  [ $DEBUG -eq 1 ] && echo "f[7]: $fh"
+  [ $DEBUG -eq 1 ] && echo "f[8]: $fi"
+  [ $DEBUG -eq 1 ] && echo "f[t]: $ft"
 done
 
-# [ $DEBUG -eq 1 ] && echo "${lanternFish[*]}" | tr ' ' ','
-echo ${#lanternFish[@]}
+echo $((fa + fb + fc + fd + fe + ff + fg + fh + fi))
 
 now=$(date +%s)
 diff=$((now-started))
